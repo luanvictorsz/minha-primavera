9061769926b292836610aadf1f6e4eb82780cfc1
victor souza <luanvitorlima2017@gmail.com>
2026-02-20 18:26:52 -0300

    fix: removendo travecoes

--- src/sobre.js
+++ src/sobre.js
@@ -23,8 +23,8 @@ export function criarPaginaSobre(container) {
     <h3>O projeto</h3>
     <p>
       Este projeto nasceu como uma forma de demonstrar o meu carinho pela minha namorada. É algo simples, mas foi feito com muito amor.
-      O Momo tem um significado especial: foi um dos presentes que comprei para ela no nosso primeiro encontro — uma bolsinha de capivara que vem com um caqui (sim, isso mesmo). Desde então, ela leva o Momo para todos os nossos encontros. Ele é tão fofinho que merecia um espacinho especial neste projeto.
-      As frases que o Momo diz são inspiradas em coisas que eu já falei para ela, em coisas que eu gostaria de ter dito, no que eu imagino que ela gostaria de ouvir — além, claro, das frases aleatórias que ela mesma solta.
+      O Momo tem um significado especial: foi um dos presentes que comprei para ela no nosso primeiro encontro, uma bolsinha de capivara que vem com um caqui (sim, isso mesmo). Desde então, ela leva o Momo para todos os nossos encontros. Ele é tão fofinho que merecia um espacinho especial neste projeto.
+      As frases que o Momo diz são inspiradas em coisas que eu já falei para ela, em coisas que eu gostaria de ter dito, no que eu imagino que ela gostaria de ouvir, além, claro, das frases aleatórias que ela mesma solta.
       No fim, é uma mistura de tudo isso. Mas, acima de qualquer detalhe, é algo feito com muito amor e carinho, pensado nela o tempo todo.
     </p>
 
